f1fd735ffbbbf1c1f2ac51b012b9f0e84700fd8a
Dawn Deshotel <deshoteldawn@gmail.com>
2020-09-10 00:25:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ function happyCustomHolidayTo(holiday, name) {
   return(`Happy ${holiday}, ${name}!`)
 }
 
-Not sure why I'm not passing the test for "Functions Lab". The IDE says it expects "It's 20 days until Mother's Day!" but the actual is "It's 20 days until Mother's Day!" So I'm not sure what I've done wrong... 
-Here is my code:
-
 function holidayCountdown(holiday, days) {
   return(`It's ${days} days unitl ${holiday}!`)
 }
