2af65c769315543f3afce7846ddbc3da40e0a1df
Dawn Deshotel <deshoteldawn@gmail.com>
2020-09-10 00:17:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // write your code below!
+
+function happyHolidays() {
+  return('Happy holidays!')
+}
+
+function happyHolidaysTo() {
+  return(`Happy holidays, ${name}!`)
+}
+
+function happyCustomHolidayTo() {
+  return(`Happy ${holiday}, ${name}!`)
+}
+
+function holidayCountdown() {
+  return(`It's ${days} days unitl ${holiday}!`)
+}
